81d2f24afd5fa1ce620b5ed1e344aedc058413ec
clement godin <clement.godin59310@gmail.com>
2016-10-28 13:54:09 +0200

    modif des nav li

--- db.php
+++ db.php
@@ -2,12 +2,9 @@
 define ("HOST","localhost");
 define ("USER","root");
 define ("PASSWORD","");
-define ("DBNAME","");//nom a rajouter
+define ("DBNAME","websitew");
 
-function Connect_db($host,$user,$password,$dbname,$options=array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"))
+function Connect_db($host,$user,$password,$dbname,$options=array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"))
 {
     $db=new PDO("mysql:host=".$host.";dbname=".$dbname,$user,$password,$options);
     return $db;

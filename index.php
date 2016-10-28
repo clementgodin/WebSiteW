c9caeac95f977732d90413f3da7d6daa53a9c033
clement godin <clement.godin59310@gmail.com>
2016-10-28 11:07:42 +0200

    ajout de fichier css et js

--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 <html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+        <link rel="stylesheet" href="normalize.css">
+        <link rel="stylesheet" href="style.css" >
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+        <title>WebSiteW</title>
     </head>
     <body>
         <?php include 'db.php'; ?>
         <?php include 'header.php'; ?>
 
         <?php include 'footer.php'; ?>
-
+        <script src="jquery-3.1.1.min.js"></script>
+        <script src="main.js"></script>
     </body>
 </html>
